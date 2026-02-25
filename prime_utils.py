0f0b0ec4ef4137a27bb08530a006995a8abe0962
Arda <85059535+ardatas@users.noreply.github.com>
2026-02-25 17:37:15 +0300

    Implemented Miller Rabin prime test to detect if a number is prime

--- prime_utils.py
+++ prime_utils.py
@@ -17,7 +17,34 @@ class PrimeUtils:
         while True:
             p = secrets.randbits(bit_len // 2) # integer division
             p += not(p & 1)
-            if MillerRabin(p):
+            if PrimeUtils.MillerRabin(p):
                 return p
+
+    @staticmethod
+    def MillerRabin(n, k=40):
+        if n == 2:
+            return True
+        if n < 2 or n % 2 == 0:
+            return False
+
+        # write n-1 as 2^r * d
+        r, d = 0, n - 1
+        while d % 2 == 0:
+            r += 1
+            d //= 2
+
+        for _ in range(k):
+            a = secrets.randbelow(n - 3) + 2  # 2 <= a <= n-2
+            x = pow(a, d, n)
+            if x == 1 or x == n - 1:
+                continue
+            for _ in range(r - 1):
+                x = pow(x, 2, n)
+                if x == n - 1:
+                    break
+            else:
+                return False
+        return True
+
 
 

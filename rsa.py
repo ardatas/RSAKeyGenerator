ce13f1275e4671c801c9163badd0423fe9d670e4
Arda <85059535+ardatas@users.noreply.github.com>
2026-02-25 18:16:43 +0300

    Fixed typo in e number

--- rsa.py
+++ rsa.py
@@ -8,7 +8,7 @@ class RSA:
     def generate_keypair():
 
         bit_len = 2048
-        e = 64473           # common choice
+        e = 65537           # common choice
 
         p = PrimeUtils.generate_prime(bit_len)
         q = PrimeUtils.generate_prime(bit_len)
@@ -21,8 +21,5 @@ class RSA:
         n = p * q
         phiN = (p - 1) * (q - 1)      # ϕ(n)=(p−1)⋅(q−1)
 
-        d = MathUtils.extended_gcd(e, phiN)  # d is the multiplicative inverse of e modulo φ(n)`
+        d = MathUtils.mod_inverse(e, phiN)  # d is the multiplicative inverse of e modulo φ(n)`
         return (e,n), (d,n)
-
-
-print(RSA.generate_keypair())
